367eb86ee3b4fa9340cb6a36fa01dd07ec8c1ce8
tipakA <emka456@onet.pl>
2020-06-13 00:29:22 +0200

    Add getPermissions function

--- src/bot/util/util.ts
+++ src/bot/util/util.ts
@@ -1,6 +1,7 @@
-import { Message } from 'discord.js'; // eslint-disable-line no-unused-vars
-import { Prefix } from './interfaces'; // eslint-disable-line no-unused-vars
+import { CheckedPermissions, Prefix } from './interfaces'; // eslint-disable-line no-unused-vars
+import { GuildMember, Message, PermissionString } from 'discord.js'; // eslint-disable-line no-unused-vars
 import { promisify } from 'util';
+import { permissions as readable } from './constants';
 import { readdir } from 'fs';
 
 export const wait = promisify(setTimeout);
@@ -14,4 +15,17 @@ export function getPrefix(message: Message): Prefix {
     prefix,
     type: 'default',
   } as Prefix;
+}
+
+export function getPermissions(member: GuildMember | null, permissions: PermissionString[]): CheckedPermissions {
+  const res: CheckedPermissions = { missing: [], missingReadable: [] };
+  if (!member) return { ...res, _error: 'MEMBER_NULL' };
+  if (!permissions.length) return res;
+
+  const missing = member.permissions.missing(permissions);
+  for (const p of missing) {
+    res.missing.push(p);
+    res.missingReadable.push(readable.en[p]);
+  }
+  return res;
 }
