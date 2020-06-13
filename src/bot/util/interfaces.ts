f0e9ec325926ba5998a3bdd736c83bce87e13dae
tipakA <emka456@onet.pl>
2020-06-13 02:30:03 +0200

    Remove not needed type

--- src/bot/util/interfaces.ts
+++ src/bot/util/interfaces.ts
@@ -2,6 +2,12 @@ import { Message, PermissionString, Snowflake } from 'discord.js'; // eslint-dis
 import { permissions } from './constants'; // eslint-disable-line no-unused-vars
 import { default as tipakBot } from '../tipakBot'; // eslint-disable-line no-unused-vars
 
+export interface BlacklistEntry {
+  id: Snowflake;
+  blacklisted: boolean;
+  notified?: boolean;
+}
+
 export interface Command {
   run(message: Message, args: string[]): Promise<Message>;
   name: string;
@@ -20,43 +26,10 @@ export interface Command {
 }
 
 export interface CheckedPermissions {
-  missing: CheckedPerm[];
+  missing: PermissionString[];
   missingReadable: string[];
   _error?: PermError;
 }
-
-export type CheckedPerm =
-  | 'CREATE_INSTANT_INVITE'
-  | 'KICK_MEMBERS'
-  | 'BAN_MEMBERS'
-  | 'ADMINISTRATOR'
-  | 'MANAGE_CHANNELS'
-  | 'MANAGE_GUILD'
-  | 'ADD_REACTIONS'
-  | 'VIEW_AUDIT_LOG'
-  | 'PRIORITY_SPEAKER'
-  | 'STREAM'
-  | 'VIEW_CHANNEL'
-  | 'SEND_MESSAGES'
-  | 'SEND_TTS_MESSAGES'
-  | 'MANAGE_MESSAGES'
-  | 'EMBED_LINKS'
-  | 'ATTACH_FILES'
-  | 'READ_MESSAGE_HISTORY'
-  | 'MENTION_EVERYONE'
-  | 'USE_EXTERNAL_EMOJIS'
-  | 'VIEW_GUILD_INSIGHTS'
-  | 'CONNECT'
-  | 'SPEAK'
-  | 'MUTE_MEMBERS'
-  | 'DEAFEN_MEMBERS'
-  | 'MOVE_MEMBERS'
-  | 'USE_VAD'
-  | 'CHANGE_NICKNAME'
-  | 'MANAGE_NICKNAMES'
-  | 'MANAGE_ROLES'
-  | 'MANAGE_WEBHOOKS'
-  | 'MANAGE_EMOJIS'
 
 export interface Event {
   run(client: tipakBot, ...args: any[]): any;
